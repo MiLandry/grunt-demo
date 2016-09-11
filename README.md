5e875319a94a3157e8f32eca7667964cf0a41a0c
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-09-11 16:34:53 -0400

    got grunt typescript working

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 add junk files into "tobecleaned" directory
 
 then run grunt -v from root of project
+
+
+#run the webserver
+node app.js
+then browset o..
+
+localhost:8080
--- gruntfile.js
+++ gruntfile.js
@@ -4,6 +4,20 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        typescript: {
+            options: {
+                module: 'commonjs'
+            },
+            all: {
+                
+                    src: ['./src/*.ts'],
+                    dest: 'dist'
+                  
+
+            }
+        },
+
+
         clean: {  //configure the "grunt-contrib-clean" task, note that this line is a property on the config object, and that its configuring (by convention?) grunt-contrib-clean, ie the last word in the task, which is loaded later
             output: ['ToBeCleaned/*'] //clean all files in the directory 'ToBeCleaned'
         }
@@ -11,7 +25,8 @@ module.exports = function (grunt) {
     });
 
     grunt.loadNpmTasks("grunt-contrib-clean"); //load 3rd party plugin from package.json
+    grunt.loadNpmTasks("grunt-typescript");
 
-    grunt.registerTask("default", ['clean']); //map plugins to runnable commands
+    grunt.registerTask("default",  ['typescript']); //map plugins to runnable commands
 
 };
